dbd1cc3ace4996b0add4730a022c86977e2c3f4d
Ancelbot <135066230+Ancelbot@users.noreply.github.com>
2023-06-29 15:27:09 -0500

    Ancelsiñho

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('Gata\nBot\nMD', {
+say('Ancelsiñho\nBot\nMD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Por Gata Dios`, {
+say(`Creado por Ancelbot`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
