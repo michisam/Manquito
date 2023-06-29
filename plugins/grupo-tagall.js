37e74f1c0c18e8bc29dacf08b9d52ccb2bdb3df8
Ancelbot <135066230+Ancelbot@users.noreply.github.com>
2023-06-29 18:22:31 -0500

    𝐀𝐧𝐜𝐞𝐥𝐬𝐢𝐧̃𝐡𝐨 💖🐈

--- plugins/grupo-tagall.js
+++ plugins/grupo-tagall.js
@@ -4,13 +4,11 @@ global.dfail('admin', m, conn)
 throw false
 }
 let pesan = args.join` `
-let oi = `ღ ${lenguajeGB['smsAddB5']()} ${pesan}`
-let teks = `╭━〔 *${lenguajeGB['smstagaa']()}* 〕━⬣\n\n${oi}\n\n`
+let oi = `*@ancelzzz* ${pesan}`
+let teks = `*🧛🏻‍♂️𝙴𝙻 𝙳𝙸𝙾𝚂 𝙰𝙽𝙲𝙴𝙻 𝙻𝙾𝚂 𝙸𝙽𝚅𝙾𝙲𝙰🧛🏻‍♂️*\n\n${oi}\n\n *🧛🏻‍♂️➢ 𝙴𝚃𝙸𝚀𝚄𝙴𝚃𝙰𝚂*\n`  
 for (let mem of participants) {
-teks += `┃⊹ @${mem.id.split('@')[0]}\n`}
-teks += `┃\n`
-teks += `┃ ${wm}\n`
-teks += `╰━━━━━[ *𓃠 ${vs}* ]━━━━━⬣`
+teks += `🍃៚@${mem.id.split('@')[0]}\n`}
+teks += `└ 𝐁𝐲 𝐀𝐧𝐜𝐞𝐥𝐮 - 𝐁𝐨𝐭\n\n*:>*`
 conn.sendMessage(m.chat, { text: teks, mentions: participants.map(a => a.id) }, )  
 }
 handler.command = /^(tagall|invocar|invocacion|todos|invocación)$/i
